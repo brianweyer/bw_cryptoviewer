b7e3b98413b98585d53b45da06aa68abb1303d1b
Brian Weyer <brianweyer@gmail.com>
2018-01-01 23:05:40 -0600

    Project setup

---
+++ cryptoviewer/cryptoviewer/MainTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MainTableViewCell.swift
+//  cryptoviewer
+//
+//  Created by Brian Weyer on 1/1/18.
+//  Copyright © 2018 Brian Weyer. All rights reserved.
+//
+
+import UIKit
+
+class MainTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
